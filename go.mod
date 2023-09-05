9219816093346c39a8f50eafa204ff56a01c7ecd
haikalsalam <mochamad.haikal@salamenterprise.com>
2023-09-05 11:53:40 +0700

    nyoba golang app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mochamadhaikal/kale-go-app
+
+go 1.20
+
+require github.com/mochamadhaikal/kale-go-modules v1.0.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	hilih "github.com/mochamadhaikal/kale-go-modules"
+)
+
+func main() {
+	output := hilih.SayHilih("Haikal")
+
+	fmt.Println(output)
+}
